93d48ca50b3f45dc707d1f1759aaded51f1fd236
melosucky <valerio.cappelloni@gmail.com>
2026-07-03 13:27:23 +0200

    document custom exceptions

--- src/document_exceptions.py
+++ src/document_exceptions.py
@@ -4,10 +4,8 @@ class DocumentLoaderError(Exception):
 class UnsupportedFormatError(DocumentLoaderError):
   """Raised when the file extension is not supported."""
  
- 
 class DocumentParsingError(DocumentLoaderError):
   """Raised when parsing/extraction of a file's contents fails."""
- 
  
 class EncodingDetectionError(DocumentLoaderError):
   """Raised when the encoding of a text file cannot be detected or decoded."""
